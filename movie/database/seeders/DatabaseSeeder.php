503d9aac850b6714ce92460b61101c2b25802970
dankabijedic <dankabijedic@gmail.com>
2022-07-03 01:01:42 +0100

    Deleted unnecessary lines of code

--- movie/database/seeders/DatabaseSeeder.php
+++ movie/database/seeders/DatabaseSeeder.php
@@ -23,47 +23,5 @@ class DatabaseSeeder extends Seeder
         User::truncate();
 
         Review::factory(5)->create();
-
-        // $user = User::factory()->create();
-
-        // $movie1 = Movie::create([
-        //     'title'=>"Title1",
-        //     'description'=>"Description1",
-        //     'director'=>"Director1"
-        // ]);
-
-        // $movie2 = Movie::create([
-        //     'title'=>"Title2",
-        //     'description'=>"Description2",
-        //     'director'=>"Director2"
-        // ]);
-
-        // $movie3 = Movie::create([
-        //     'title'=>"Title3",
-        //     'description'=>"Description3",
-        //     'director'=>"Director3"
-        // ]);
-
-        // $review1 = Review::create([
-        //     'rating'=>"8",
-        //     'comment'=>"Comment1",
-        //     'user_id'=>$user->id,
-        //     'movie_id'=>$movie1->id
-        // ]);
-
-        // $review2 = Review::create([
-        //     'rating'=>"8",
-        //     'comment'=>"Comment1",
-        //     'user_id'=>$user->id,
-        //     'movie_id'=>$movie2->id
-        // ]);
-
-        // $review3 = Review::create([
-        //     'rating'=>"8",
-        //     'comment'=>"Comment1",
-        //     'user_id'=>$user->id,
-        //     'movie_id'=>$movie3->id
-        // ]);
-        
     }
 }
